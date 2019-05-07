9890b20d3c62be8f597259134667f0e4ccee47a3
jamescorrenti <james.e.correnti@gmail.com>
2019-05-06 20:28:28 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -36,10 +36,10 @@ def create_app(config_name):
         from flask_sslify import SSLify
         sslify = SSLify(app)
 
-    """
+    
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    """
+    
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
